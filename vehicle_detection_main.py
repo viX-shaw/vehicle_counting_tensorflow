2fd06868b024786025a9d9091d90ffa0356efa98
VIVEK <vivekkumarshaw9@gmail.com>
2019-05-24 17:51:53 +0530

    wip

--- vehicle_detection_main.py
+++ vehicle_detection_main.py
@@ -183,10 +183,10 @@ def object_detection_function():
                     )
 
                 # when the vehicle passed over line and counted, make the color of ROI line green
-                if counter == 1:
-                    cv2.line(input_frame, (0, 420), (1280, 420), (0, 0xFF, 0), 5)
-                else:
-                    cv2.line(input_frame, (0, 420), (1280, 420), (0, 0, 0xFF), 5)
+                # if counter == 1:
+                #     cv2.line(input_frame, (0, 420), (1280, 420), (0, 0xFF, 0), 5)
+                # else:
+                #     cv2.line(input_frame, (0, 420), (1280, 420), (0, 0, 0xFF), 5)
 
                 # insert information text to video frame
                 cv2.rectangle(input_frame, (10, 275), (230, 337), (180, 132, 109), -1)
