d799ffc67163366ce1b6a16077b0e7a8aa382b29
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-18 17:45:53 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -63,7 +63,7 @@ def add_new_object(obj, image, counters, trackers, name, curr_frame):
     if success:
         feature = feature_generator(image, [(xmin, ymin, xmax-xmin, ymax-ymin)])
         # print("Adding feature to new track object", np.asarray(feature).shape)
-        trackers.append((tracker, label, age, [feature]))
+        trackers.append([tracker, label, age, [feature]])
     label_object(RED, RED, fontface, image, label, textsize, 4, xmax, xmid, xmin, ymax, ymid, ymin)
 
 def not_tracked(image, object_, boxes, trackers):
@@ -154,7 +154,7 @@ def update_trackers(image, counters, trackers, curr_frame):
             f.write("Tracker no {} : {}, age {}\n".format(n, distance, age))
 
         if abs(distance) > 2.2:
-            print("Working")
+            # print("Working")
             #needs the whole track object
             pair[2]+=1
 
