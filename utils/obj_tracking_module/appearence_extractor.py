1697f7b52b51b9f13646f6d45b5225c7e705bc35
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-19 03:05:01 +0530

    wip

--- utils/obj_tracking_module/appearence_extractor.py
+++ utils/obj_tracking_module/appearence_extractor.py
@@ -65,6 +65,7 @@ def extract_image_patch(image, bbox, patch_shape):
     sx, sy, ex, ey = bbox
     image = image[sy:ey, sx:ex]
     image = cv2.resize(image, tuple(patch_shape[::-1]))
+    cv2.imsave(image, "/content/sample_data/{}.jpg".format(''.join(random.choices(string.ascii_uppercase + string.digits, k=4))))
     return image
 
 
