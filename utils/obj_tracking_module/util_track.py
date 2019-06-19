4808a94f30184aae186a46f7c5d5f4aeacdae08d
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-19 14:10:43 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -73,7 +73,7 @@ def not_tracked(image, object_, boxes, trackers):
         # return []  # No new classified objects to search for
         return False
     cv2.imwrite("/content/data/{}.jpg".format((
-        ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))), image)
+        ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))), image))
 
     ymin, xmin, ymax, xmax = object_
     new_objects = []
