f635a51577e040d2a761bd5b8dc3d43593b13423
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-20 15:08:13 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -164,8 +164,8 @@ def update_trackers(image, cp_image, counters, trackers, curr_frame):
             # print("Working")
             #needs the whole track object
             pair[2]+=1
-        else:
-            pair[3].append(dt_feature)
+        # else:
+        #     pair[3].append(dt_feature)
 
         if age >= 30:
             print("Deleting tracker {} with age {} on AOI exit..".format(car, age))
