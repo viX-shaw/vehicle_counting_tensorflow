b5efc1e26acc1ee73754786aaf78bc6dc14a7e1a
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-21 00:03:07 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -95,6 +95,7 @@ def not_tracked(image, object_, boxes, trackers):
         bymid = int((bymin + bymax) / 2)
         dist = math.sqrt((xmid - bxmid)**2 + (ymid - bymid)**2)
         if dist >= box_range:
+            pass
             # print("Car no {} is {}units, range is {}".format(car_no, dist, box_range))
         if dist <= box_range:
             # print("car no ", car_no, "is in range")
