fb5bb064b98f75a2c5e7990ac8a077a77b5d5540
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-20 11:54:26 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -294,7 +294,7 @@ def _pdist(a, b):
     # a, b = np.asarray(a), np.asarray(b)
     if len(a) == 0 or len(b) == 0:
         return np.zeros((len(a), len(b)))
-    a2, b2 = np.square(a).sum(axis=0), np.square(b).sum(axis=0)
+    a2, b2 = np.square(a).sum(axis=1), np.square(b).sum(axis=1)
     r2 = -2. * np.dot(a, b.T) + a2[:, None] + b2[None, :]
     r2 = np.clip(r2, 0., float(np.inf))
     return r2
