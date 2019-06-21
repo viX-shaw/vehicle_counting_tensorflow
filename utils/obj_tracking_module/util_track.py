0b29aef62d167624c0c3d5f9ebbb44005cf27021
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-21 11:50:47 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -115,7 +115,7 @@ def not_tracked(image, object_, boxes, trackers):
             t[3].append(dt_feature)
             break
     else:
-        ymin, xmin, ymax, xmax = int(object_)
+        ymin, xmin, ymax, xmax = [int(en) for en in object_]
         dt_ft = feature_generator(image, [(xmin, ymin, xmax-xmin, ymax-ymin)])
         for x, (bx, cn, ft) in enumerate(boxes):
 
