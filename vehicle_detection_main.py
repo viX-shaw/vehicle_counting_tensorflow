6ed390af9f8bf8ed3031aa99325d841273f31d20
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-26 14:37:27 +0530

    wip

--- vehicle_detection_main.py
+++ vehicle_detection_main.py
@@ -137,7 +137,8 @@ def object_detection_function():
     masks = None
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
-            all_tensor_names = detection_graph.get_operations()
+            ops = detection_graph.get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
             # Definite input and output Tensors for detection_graph
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
@@ -152,8 +153,8 @@ def object_detection_function():
 
             if 'detection_masks:0' in all_tensor_names:
                 detection_masks = detection_graph.get_tensor_by_name('detection_masks:0')
-            detection_masks = tf.squeeze(detection_masks, [0])
-            detection_boxs = tf.squeeze(detection_boxes, [0])
+                detection_masks = tf.squeeze(detection_masks, [0])
+                detection_boxs = tf.squeeze(detection_boxes, [0])
             # for all the frames that are extracted from input video
             while cap.isOpened():
                 (ret, frame) = cap.read()
@@ -175,16 +176,18 @@ def object_detection_function():
 
                 # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
                 if cap.get(1) % params.sr == 0:
-                    image_np_expanded = np.expand_dims(copy_frame, axis=0)
-                    detection_masks_reframed = util_track.reframe_box_masks_to_image_masks(
-                            detection_masks, detection_boxs, copy_frame.shape[0], copy_frame.shape[1])
-                    detection_masks_reframed = tf.cast(
-                        tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                    # Follow the convention by adding back the batch dimension
-                    detection_masks = tf.expand_dims(
-                        detection_masks_reframed, 0)
                     # Actual detection.
                     if 'detection_masks:0' in all_tensor_names:
+                        image_np_expanded = np.expand_dims(copy_frame, axis=0)
+                        detection_masks_reframed = util_track.reframe_box_masks_to_image_masks(
+                                detection_masks, detection_boxs, copy_frame.shape[0], copy_frame.shape[1])
+                        
+                        detection_masks_reframed = tf.cast(
+                            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                        # Follow the convention by adding back the batch dimension
+                        detection_masks = tf.expand_dims(
+                            detection_masks_reframed, 0)
+
                         (boxes, scores, classes, num, masks) = \
                             sess.run([detection_boxes, detection_scores,
                                     detection_classes, num_detections, detection_masks],
