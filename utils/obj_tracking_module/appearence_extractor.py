22fd4a496f13174090f2068a475d436a2fc891cc
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-27 17:09:45 +0530

    wip

--- utils/obj_tracking_module/appearence_extractor.py
+++ utils/obj_tracking_module/appearence_extractor.py
@@ -121,6 +121,7 @@ def create_box_encoder(model_filename, input_name="images",
             patch = None
             if mask is not None:
                 rgb = ImageColor.getrgb('gray')
+                pil_image = Image.fromarray(image).copy()
                 solid_color = np.expand_dims(np.ones_like(mask), axis=2) * np.reshape(list(rgb), [1, 1, 3])
                 pil_solid_color = Image.fromarray(np.uint8(solid_color)).convert('RGBA')
                 pil_mask = Image.fromarray(np.uint8(255.0*(np.ones_like(mask)-mask))).convert('L')
