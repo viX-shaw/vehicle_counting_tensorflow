078b023278b95eeccb9f02ed0d20d20ce0ca7650
VIVEK <vivekkumarshaw9@gmail.com>
2019-06-27 17:42:40 +0530

    wip

--- utils/obj_tracking_module/appearence_extractor.py
+++ utils/obj_tracking_module/appearence_extractor.py
@@ -122,13 +122,15 @@ def create_box_encoder(model_filename, input_name="images",
             if mask is not None:
                 rgb = ImageColor.getrgb('gray')
                 pil_image = Image.fromarray(image).copy()
+                image = np.array(pil_image.getdata()).reshape(
+                             (pil_image.size[0], pil_image.size[1], 3))
                 solid_color = np.expand_dims(np.ones_like(mask), axis=2) * np.reshape(list(rgb), [1, 1, 3])
                 pil_solid_color = Image.fromarray(np.uint8(solid_color)).convert('RGBA')
                 pil_mask = Image.fromarray(np.uint8(255.0*(np.ones_like(mask)-mask))).convert('L')
-                image = Image.composite(pil_solid_color, pil_image, pil_mask)
-                image = np.array(image.getdata()).reshape((image.size[0], image.size[1], 3))
-                cv2.imwrite("/content/sample_data/{}.jpg".format(
-                    ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))), image)
+                p_image = Image.composite(pil_solid_color, pil_image, pil_mask)
+                np.copyto(image, np.array(p_image.convert('RGB')))
+                # cv2.imwrite("/content/sample_data/{}.jpg".format(
+                #     ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))), image)
             
             patch = extract_image_patch(image, box, image_shape[:2])
             if patch is None:
