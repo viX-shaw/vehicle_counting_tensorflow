1a47d61cc2545eaa1e08b679457ea53c2a8137a5
VIVEK <vivekkumarshaw9@gmail.com>
2019-07-02 13:15:49 +0530

    wip

--- vehicle_detection_main.py
+++ vehicle_detection_main.py
@@ -72,7 +72,7 @@ except Exception as e:
     pass
 # input video
 cap = cv2.VideoCapture(params.file)
-print(cap.get(cv2.CV_CAP_PROP_FRAME_WIDTH), cap.get(cv2.CV_CAP_PROP_FRAME_HEIGHT))
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 
 
@@ -167,8 +167,8 @@ def object_detection_function():
                 detection_boxs = tf.slice(detection_boxs, [0, 0], [real_num_detection, -1])
                 detection_mks = tf.slice(detection_mks, [0, 0, 0], [real_num_detection, -1, -1])
                 
-                width = cap.get(cv2.CV_CAP_PROP_FRAME_WIDTH)   # float
-                height = cap.get(cv2.CV_CAP_PROP_FRAME_HEIGHT) # float
+                width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+                height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
 
                 detection_masks_reframed = util_track.reframe_box_masks_to_image_masks(
                         detection_mks, detection_boxs, height, width)
