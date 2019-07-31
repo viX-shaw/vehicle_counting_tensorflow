ca11c4bafb9b9eb39e64a5ddd77766fc4c36dbd9
VIVEK <vivekkumarshaw9@gmail.com>
2019-07-31 16:34:52 +0530

    wip

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -549,7 +549,7 @@ def visualize_boxes_and_labels_on_image_array(current_frame_number,image,
             
             #tr_id receives the id of the tracker that matches the passed in bbox, otherwise returns -1
             not_tracked, tr_id = util_track.not_tracked(image, (top, left, bottom, right),
-                trackers, tracker_name, eu_threshold, str(current_frame_number)[:-2], metric, iou_threshold, mask):
+                trackers, tracker_name, eu_threshold, str(current_frame_number)[:-2], metric, iou_threshold, mask)
 
             if not_tracked:  
               # generating detections for deep-mot-sort
