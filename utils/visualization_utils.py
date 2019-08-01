faa8e84dd4e736dd16e018db48c462a314423983
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-01 14:27:21 +0530

    wip

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -507,22 +507,22 @@ def visualize_boxes_and_labels_on_image_array(current_frame_number,image,
 
   # Update all tracked boxes from previous frame
   # tracker_boxes = util_track.update_trackers(image, counters, trackers)
-  # nms_boxes = [(xmin, ymin, xmax-xmin, ymax-ymin) for (ymin, xmin, ymax, xmax), _ in box_to_color_map.items()]
-  # nms_indices = non_max_suppression(nms_boxes, 0.35)
+  nms_boxes = [(xmin, ymin, xmax-xmin, ymax-ymin) for (ymin, xmin, ymax, xmax), _ in box_to_color_map.items()]
+  nms_indices = non_max_suppression(nms_boxes, 0.7)
 
   # print(len(nms_boxes), len(nms_indices))
   # nms_boxes = box_to_color_map.items()
-  # dt_boxes = []
-  # for i, (box, c) in enumerate(box_to_color_map.items()):
-  #   if i in nms_indices:
-  #     dt_boxes.append(box)
+  dt_boxes = []
+  for i, (box, c) in enumerate(box_to_color_map.items()):
+    if i in nms_indices:
+      dt_boxes.append(box)
   # Draw all boxes onto image.
   # for box, m in box_to_instance_masks_map.items():
   #   print(m.shape, np.count_nonzero(m))
   # trackers = deepcopy(trackers)
   mapped_tr_ids = []
-  # for box in dt_boxes:
-  for box, c in box_to_color_map.items():
+  for box in dt_boxes:
+  # for box, c in box_to_color_map.items():
     # if int(current_frame_number) % 100 == 0:
       # print("BBoxes in frame",int(current_frame_number), "is" ,len(list(box_to_color_map)))
     ymin, xmin, ymax, xmax = box
