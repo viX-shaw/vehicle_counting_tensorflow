15df3628a87d2442a5ba9923e7f7882758f23103
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-02 18:18:21 +0530

    wip

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -563,7 +563,7 @@ def visualize_boxes_and_labels_on_image_array(current_frame_number,image,
               # np.copyto(image, np.array(image_pil))
 
               counters[display_str_list[0][:-5]]+=1
-              util_track.add_new_object([(top, left, bottom, right)], image, counters,
+              util_track.add_new_object((top, left, bottom, right), image, counters,
                 trackers, tracker_name, str(current_frame_number)[:-2], mask)
             # else:
             #   mapped_tr_ids.append(tr_id)
