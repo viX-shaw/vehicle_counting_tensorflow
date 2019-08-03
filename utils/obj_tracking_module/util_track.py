a6c3fc44dae19d489bb590150985c09f4830194f
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-03 14:33:22 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -138,19 +138,14 @@ def not_tracked(image, object_, trackers, name, threshold, curr_frame_no,
     if min_id != -1:
         t=trackers[min_id]
         t[3]=0 #Resetting age on detection
-        # tr = OPENCV_OBJECT_TRACKERS[name]()
-        # success = tr.init(image, (xmin, ymin, xmax-xmin, ymax-ymin))
-        # if mask is not None:
-            # try:
-            #     tr.setInitialMask(mask)
-            # except Exception as e:
-            #     pass
-                # warnings.warn(str(e))
-        # if success:
+        tr = OPENCV_OBJECT_TRACKERS[name]()
+        success = tr.init(image, (xmin, ymin, xmax-xmin, ymax-ymin))
+        
+        if success:
             # with open('./Re-identification.txt', 'a') as f:
             #     f.write("Updating tracker {} in frame {}\n".format(t[2], curr_frame_no))
             # del t[0]
-        # t[0] = tr             #uncomment 
+        t[0] = tr             #uncomment 
         dt_feature = feature_generator(image, [(xmin, ymin, xmax-xmin, ymax-ymin)], mask)
         t[4].append(dt_feature)
             # t[-1] = True
@@ -176,17 +171,11 @@ def not_tracked(image, object_, trackers, name, threshold, curr_frame_no,
             tr = OPENCV_OBJECT_TRACKERS[name]()
             # print((xmin, ymin, xmax-xmin, ymax-ymin))
             success = tr.init(image, (xmin, ymin, xmax-xmin, ymax-ymin))
-            # if mask is not None:
-                # try:
-                #     tracker.setInitialMask(mask)
-                # except Exception as e:
-                #     pass
-                    # warnings.warn(str(e))
+            
             if success:
                 # with open('./Re-identification.txt', 'a') as f:
                 #     f.write("Re-initializing tracker {} in frame {}\n".format(t[2], curr_frame_no))
                 # print("Re-initializing tracker ",cn, t[2])
-                # del t[0]
                 t[0] = tr
                 t[3] = 0
                 t[4].append(dt_ft)
