4d316dbb6fd2d946c046125b79b45b16a6d25027
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-05 17:39:52 +0530

    wip

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup  
 from Cython.Build import cythonize  
 setup(ext_modules = cythonize(["./utils/obj_tracking_module/appearence_extractor.py",
-"./utils/obj_tracking_module/util_track.py", "./utils/visualization_utils.py"], annotate=True))  
+"./utils/obj_tracking_module/util_track.pyx", "./utils/visualization_utils.py"], annotate=True))  
