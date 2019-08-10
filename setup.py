711385c65ebdf072539ccb7d04780e86b7e484f7
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-10 15:54:36 +0530

    Wip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize  
 import numpy
 setup(ext_modules = cythonize(["./utils/obj_tracking_module/appearence_extractor.py",
-"./utils/obj_tracking_module/util_track.py",
+"./utils/obj_tracking_module/util_track.pyx",
  "./utils/obj_tracking_module/util_track.pxd",
  "./utils/visualization_utils.py",
  "vehicle_detection_main.py"], annotate=True), include_dirs = [numpy.get_include()])  
