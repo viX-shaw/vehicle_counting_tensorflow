8496415b30c98007ce9d67827db02344a7cca8fd
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-11 23:13:45 +0530

    wip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 import numpy
 setup(ext_modules = cythonize(["./utils/obj_tracking_module/appearence_extractor.py",
 "./utils/obj_tracking_module/util_track.pyx",
- "./utils/obj_tracking_module/util_track.pxd",,
+ "./utils/obj_tracking_module/util_track.pxd",
  "./utils/visualization_utils.py",
  "vehicle_detection_main.py"], annotate=True), include_dirs = [numpy.get_include()],
  include_path=["./utils/obj_tracking_module"])  
