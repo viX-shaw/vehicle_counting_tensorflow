01b51b4229586103a4f3aacde1c0b36cb2bccfb4
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-11 23:25:29 +0530

    wip

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ setup(ext_modules = cythonize(["./utils/obj_tracking_module/appearence_extractor
  "./utils/obj_tracking_module/util_track.pxd",
  "./utils/visualization_utils.py",
  "vehicle_detection_main.py"], annotate=True), include_dirs = [numpy.get_include()],
- include_path=["./utils/obj_tracking_module"])  
+ package_data = {'utils': ['obj_tracking_module/*.pxd']})  
