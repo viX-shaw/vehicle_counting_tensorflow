fdda7df3586420aadece813212a8b36232975d95
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-12 00:29:27 +0530

    wip

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -32,16 +32,16 @@ from utils.image_utils import image_saver
 
 #  predicted_speed predicted_color module - import
 from utils.speed_and_direction_prediction_module import speed_prediction
-# from utils.object_counting_module import object_counters
+from utils.object_counting_module import object_counters
 
 from utils.obj_tracking_module import util_track
 
 # color recognition module - import
 from utils.color_recognition_module import color_recognition_api
 
-cimport utils.obj_tracking_module.util_track as util_track
+from utils.obj_tracking_module.util_track cimport Info
 
-from util_track cimport Info
+# from util_track cimport Info
 
 
 # Variables
