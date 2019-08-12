95b77bbb2f1539a4f9118729b9b561d403df7b21
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-12 12:18:28 +0530

    wip

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -35,7 +35,7 @@ from utils.speed_and_direction_prediction_module import speed_prediction
 from utils.object_counting_module import object_counters
 
 # from utils.obj_tracking_module import util_track
-from utils.obj_tracking_module.util_track cimport bbox
+cdef (int, int ,int ,int) bbox
 cimport utils.obj_tracking_module.util_track as util_track
 
 # color recognition module - import
