f01d9fb16407e197a136c3686bc96c778b4bb108
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-14 00:32:58 +0530

    wip

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from distutils.core import setup  
 from Cython.Build import cythonize  
 import numpy
-setup(ext_modules = cythonize(["./utils/obj_tracking_module/appearence_extractor.py",
-"./utils/obj_tracking_module/util_track.pyx",
+setup(ext_modules = cythonize(["./utils/obj_tracking_module/util_track.pyx",
  "./utils/obj_tracking_module/util_track.pxd",
  "./utils/visualization_utils.pyx"], annotate=True), include_dirs = [numpy.get_include()],
  package_data = {'vehicle_counting_tensorflow/utils/obj_tracking_module': ['*.pxd']})  
