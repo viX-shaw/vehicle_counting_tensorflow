99137282c34dae6c0037ede50259011f0a04f660
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-14 05:39:03 +0530

    wip

--- setup.py
+++ setup.py
@@ -11,6 +11,5 @@ extra_compile_args = ["-g3", "-O0", "-DDEBUG=%s" % _DEBUG_LEVEL, "-UNDEBUG"]
 exttension = Extension("util_track",["./utils/obj_tracking_module/util_track.pyx"],
                 extra_compile_args=extra_compile_args)
 setup(ext_modules = cythonize([exttension,
- "./utils/obj_tracking_module/util_track.pxd",
  "./utils/visualization_utils.pyx"], annotate=True), include_dirs = [numpy.get_include()],
  package_data = {'vehicle_counting_tensorflow/utils/obj_tracking_module': ['*.pxd']})  
