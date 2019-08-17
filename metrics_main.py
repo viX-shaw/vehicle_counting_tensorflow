6671a7574f03693337e26c1d5e752741fdf59369
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-17 14:51:59 +0530

    wip

--- metrics_main.py
+++ metrics_main.py
@@ -111,7 +111,13 @@ def object_detection_function():
     total_passed_vehicle = 0
     lost_trackers = 0
     trackers = []
-    counters = defaultdict()
+    counters = {
+    "person": 0,
+    "car": 0,
+    "truck":0,
+    "bus":0,
+    "lost_trackers": 0
+    }
     speed = 'waiting...'
     direction = 'waiting...'
     size = 'waiting...'
