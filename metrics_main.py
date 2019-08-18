b85663df9eed124818fe644a78286076fff9fd34
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-18 22:05:10 +0530

    wip

--- metrics_main.py
+++ metrics_main.py
@@ -134,12 +134,12 @@ def object_detection_function():
         boxes ,scores, classes = get_detboxes_classes_and_scores(detections, key)
         # print("boxs , scores, classes shapes", boxes.shape, scores.shape, classes.shape, key )
         # (ret, frame) = cap.read()
-        input_frame = np.asarray(Image.open(entry).convert('RGB'))
+        input_frame = np.asarray(Image.open(entry).convert('BGR'))
         copy_frame = input_frame.copy()
         # if not ret:
         #     print ('end of the video file...')
         #     break
-        # cv2.imwrite('/content/1.jpg', input_frame)
+        cv2.imwrite('/content/1.jpg', input_frame)
         # break
 
         # input_frame = frame
