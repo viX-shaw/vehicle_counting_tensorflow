c4755382ae6e2f3d63a22958a3c335149ded8676
VIVEK <vivekkumarshaw9@gmail.com>
2019-08-18 22:42:14 +0530

    wip

--- utils/obj_tracking_module/util_track.py
+++ utils/obj_tracking_module/util_track.py
@@ -79,9 +79,9 @@ def add_new_object(obj, image, counters, trackers, name, curr_frame, seq_dir, ma
             feature = feature_generator(image, [(xmin, ymin, xmax-xmin, ymax-ymin)])
         # print("Adding feature to new track object", np.asarray(feature).shape)
         trackers.append([tracker, (xmin, ymin, xmax-xmin, ymax-ymin), label, age, [feature], success])
-        # with open(seq_dir+"/result.txt", "a") as f:
-        #     f.write('{:d},{:d},{:.2f},{:.2f},{:.2f},{:.2f},1,-1,-1,-1\n'.format(
-        #     int(curr_frame), int(label), obj[1], obj[0], obj[3]-obj[1], obj[2]-obj[0]))
+        with open(seq_dir+"/result.txt", "a") as f:
+            f.write('{:d},{:d},{:.2f},{:.2f},{:.2f},{:.2f},1,-1,-1,-1\n'.format(
+            int(curr_frame), int(label), obj[1], obj[0], obj[3]-obj[1], obj[2]-obj[0]))
         # print("Car - ", label, "is added")
         # label_object(RED, RED, fontface, image, label, textsize, 4, xmax, xmid, xmin, ymax, ymid, ymin)
 
@@ -152,9 +152,9 @@ def not_tracked(image, object_, trackers, name, threshold, curr_frame_no,
             t[0] = tr             #uncomment 
             dt_feature = feature_generator(image, [(xmin, ymin, xmax-xmin, ymax-ymin)], mask)
             t[4].append(dt_feature)
-            # with open(seq_dir+"/result.txt", "a") as f:
-            #     f.write('{:d},{:d},{:.2f},{:.2f},{:.2f},{:.2f},1,-1,-1,-1\n'.format(
-            #     int(curr_frame_no), int(t[2]), object_[1], object_[0], object_[3]-object_[1], object_[2]-object_[0]))
+            with open(seq_dir+"/result.txt", "a") as f:
+                f.write('{:d},{:d},{:.2f},{:.2f},{:.2f},{:.2f},1,-1,-1,-1\n'.format(
+                int(curr_frame_no), int(t[2]), object_[1], object_[0], object_[3]-object_[1], object_[2]-object_[0]))
             # t[-1] = True
     else:
         # ymin, xmin, ymax, xmax = [int(en) for en in object_]
@@ -187,9 +187,9 @@ def not_tracked(image, object_, trackers, name, threshold, curr_frame_no,
                 t[3] = 0
                 t[4].append(dt_ft)
                 t[-1] = True
-                # with open(seq_dir+"/result.txt", "a") as f:
-                #     f.write('{:d},{:d},{:.2f},{:.2f},{:.2f},{:.2f},1,-1,-1,-1\n'.format(
-                #     int(curr_frame_no), int(t[2]), object_[1], object_[0], object_[3]-object_[1], object_[2]-object_[0]))
+                with open(seq_dir+"/result.txt", "a") as f:
+                    f.write('{:d},{:d},{:.2f},{:.2f},{:.2f},{:.2f},1,-1,-1,-1\n'.format(
+                    int(curr_frame_no), int(t[2]), object_[1], object_[0], object_[3]-object_[1], object_[2]-object_[0]))
             
                 # break
         else:
