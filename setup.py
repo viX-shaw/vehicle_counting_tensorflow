51bb9cf5faa5fad8f161c1353f4e9a0016cd49b2
VIVEK <vivekkumarshaw9@gmail.com>
2019-09-11 17:23:03 +0530

    wip

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ import numpy
 setup(ext_modules = cythonize(["./utils/obj_tracking_module/util_track.pyx",
  "./utils/obj_tracking_module/util_track.pxd",
  "./utils/visualization_utils.pyx"], annotate=True, gdb_debug = True), include_dirs = [numpy.get_include()],
- package_data = {'vehicle_counting_tensorflow/utils/obj_tracking_module': ['*.pxd']})  
+ package_data = {'vehicle_counting_tensorflow/utils/obj_tracking_module': ['*.pxd']},
+ library_dirs = ['/content/E/vehicle_counting_tensorflow/utils/obj_tracking_module'],  
+ libraries = ['KCF', 'cvt'])
 
 # _DEBUG_LEVEL = 0
 # extra_compile_args = ["-g3", "-O0", "-DDEBUG=%s" % _DEBUG_LEVEL, "-UNDEBUG"]
