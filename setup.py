a3011426d2d001047875886c9036f1bc071c942a
VIVEK <vivekkumarshaw9@gmail.com>
2019-09-11 18:00:50 +0530

    wip

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup, Extension  
 from Cython.Build import cythonize  
+from Cython.Distutils import build_ext
 import numpy
 
-setup(ext_modules = cythonize(["./utils/obj_tracking_module/util_track.pyx",
- "./utils/obj_tracking_module/util_track.pxd",
- "./utils/visualization_utils.pyx"], annotate=True, gdb_debug = True), include_dirs = [numpy.get_include()],
- package_data = {'vehicle_counting_tensorflow/utils/obj_tracking_module': ['*.pxd']},
- library_dirs = ['/content/E/vehicle_counting_tensorflow/utils/obj_tracking_module'],  
- libraries = ['KCF', 'cvt'])
+# setup(ext_modules = cythonize(["./utils/obj_tracking_module/util_track.pyx",
+#  "./utils/obj_tracking_module/util_track.pxd",
+#  "./utils/visualization_utils.pyx"], annotate=True, gdb_debug = True), include_dirs = [numpy.get_include()],
+#  package_data = {'vehicle_counting_tensorflow/utils/obj_tracking_module': ['*.pxd']},
+#  library_dirs = ['/content/E/vehicle_counting_tensorflow/utils/obj_tracking_module'],  
+#  libraries = ['KCF', 'cvt'])
 
 # _DEBUG_LEVEL = 0
 # extra_compile_args = ["-g3", "-O0", "-DDEBUG=%s" % _DEBUG_LEVEL, "-UNDEBUG"]
@@ -16,3 +17,22 @@ setup(ext_modules = cythonize(["./utils/obj_tracking_module/util_track.pyx",
 # setup(ext_modules = cythonize([exttension,
 #  "./utils/visualization_utils.pyx"], annotate=True), include_dirs = [numpy.get_include()],
 #  package_data = {'vehicle_counting_tensorflow/utils/obj_tracking_module': ['*.pxd']})  
+
+libdr = ['/content/E/vehicle_counting_tensorflow/utils/obj_tracking_module']
+incdr = [numpy.get_include()]
+
+ext = [
+    Extension('util_track', ['utils/obj_tracking_module/util_track.pyx'],
+        libraries = ['KCF', 'cvt'],
+        library_dirs = libdr,
+        include_dirs = incdr
+        ),
+    Extension('visualization_utils', ['utils/visualization_utils.pyx'],
+        include_dirs = incdr
+        )
+]
+
+setup(
+    cmdclass = {'build_ext': build_ext}
+	ext_modules = ext
+)
